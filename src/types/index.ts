f8536b7dce92080742cb6bcd52a2c1fbbf3edd0e
Muhammad Minhajul Alam <minhajulalam.muhammad@gmail.com>
2026-03-10 23:22:10 +0600

    initial project setup - Next.js + TypeScript + Tailwind

---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+// types/index.ts
+export type Attorney = {
+  name: string
+  title: string
+  specialization: string
+  image: string
+}
+
+export type PracticeArea = {
+  title: string
+  description: string
+  icon: string
+}
