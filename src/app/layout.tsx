12b03e1d04b98f48897d3b9a4d6946d7194abefb
Muhammad Minhajul Alam <minhajulalam.muhammad@gmail.com>
2026-03-11 08:25:41 +0600

    add SVG favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,13 @@
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
 import "./globals.css";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  icons: {
+    icon: "/icon.svg",
+  },
+};
 
 export default function RootLayout({
   children,
